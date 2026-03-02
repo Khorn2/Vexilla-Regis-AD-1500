28878ce2bec20ea0f1b6551583aed4ee7414ab1a
Khorn2 <michal.tejwan@gmail.com>
2026-03-02 17:13:25 +0100

    Utworzenie Gridu i set up 1
    Stworzyłem testowy Grid, który później łatwo będzie można edytować.

---
+++ Vexilla Regis AD 1500/Assets/Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+    [SerializeField] private Tile tilePrefab;
+
+    [SerializeField] private Transform cam;
+
+    private Dictionary<Vector2, Tile> _tiles;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, Tile>();
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+
+        cam.transform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
+    }
+
+    public Tile GetTileAtPosition(Vector2 position)
+    {
+        if (_tiles.TryGetValue(position, out var tile))
+        {
+            return tile;
+        }
+        return null;
+    }
+}
